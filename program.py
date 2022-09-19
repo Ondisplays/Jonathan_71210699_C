ed33ea80ed3bb191f8e61bd5f1303ac7f111de38
Ondisplays <93306165+Ondisplays@users.noreply.github.com>
2022-09-19 20:30:31 +0700

    Update program.py

--- program.py
+++ program.py
@@ -2,15 +2,22 @@ import json
 with open('mahasiswa.json','r') as file:
     data =  json.load(file)
 jumlah = int(input('Masukan jumlah mahasiswa baru : '))
+list = []
+dict = {}
+hobitotal = []
 for i in range(jumlah):
     nama = input('Masukan nama Anda : ')
+    list.append(nama)
     hobi = int(input('Masukan Jumlah hobi : '))
     for x in range (1,hobi+1):
-        hobitotal = []
+
         hobi = (input('Masukan Hobi ke-'+ str(x) + ' : '))
         hobitotal.append(hobi)
     prestasi = input('Masukan Prestasi Anda : ')
     print('=== DATA berhasil ditambahkan === \n')
 
-with open('mahasiswa.json', 'w') as file:
-    json.dump(data, file)
+    list.append({"BioData":{"Hobi":hobitotal,"Prestasi":prestasi}})
+data[nama] = list
+
+with open('mahasiswa.json', 'w') as datafile:
+    json.dump(data, datafile)
